20745fa2c0c36381455bcf611a57b8fc2844300d
SundayJoshua <sundayjosh07@gmail.com>
2025-09-23 01:11:13 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,9 @@ import '@scalar/api-reference-react/style.css'
 function App() {
   return (
     <ApiReferenceReact
-      configuration={{
-        spec: {
-          url: 'https://api.api-fiddle.com/v1/public/resources/oas_api_3_1/sunday-joshuas-organization-823/dry-kangaroo-frsa?draft_id=01997346-7d62-7192-9b17-272a917036d7',
-        },
-      }}
+    configuration={{
+        url: 'https://api.api-fiddle.com/v1/public/resources/oas_api_3_1/sunday-joshuas-organization-823/dry-kangaroo-frsa?draft_id=01997346-7d62-7192-9b17-272a917036d7',
+    }}
     />
   )
 }
